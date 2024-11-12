ca34456bad5875dc281ccdd87b19c9228d59db36
Anton Faren <Anton.faren@elev.ga.ntig.se>
2024-11-12 13:22:27 +0100

    first commit

---
+++ 01/Program.cs
@@ -0,0 +1,74 @@
+﻿
+// Första saken:
+// if(6 > 3){
+// Console.WriteLine("Hello, World!");
+// }
+
+// andra saken och tredje :
+
+// Console.WriteLine("username:");
+// string namn = Console.ReadLine();
+// if (namn == "kalleanka")
+// {
+//     Console.WriteLine("password:");
+//     string lösenord = Console.ReadLine();
+//     if (lösenord == "12345")
+//     {
+//         Console.WriteLine("welcome");
+//     }
+//     else
+//     {
+//         Console.WriteLine("wrong password");
+//     }
+// }
+// else
+// {
+//     Console.WriteLine("wrong username");
+// }
+
+// fjärde saken:
+// int times = 0;
+// while (times < 33)
+// {
+//     Console.WriteLine("hello world");
+//     times = +1;
+
+// }
+
+// femte saken :
+// while (true)
+// {
+//     Console.WriteLine("lösenord:");
+//     string lösenord = Console.ReadLine();
+
+//     if(lösenord == "12345"){
+//         break ;
+//     }
+
+// }
+
+// 6 saken :
+// int siffra = 0;
+// bool success = true;
+
+
+// for (int gånger = 0; gånger < 5; gånger++)
+// {
+
+//     Console.WriteLine("nummer?");
+//     string nummer = Console.ReadLine();
+
+//     success = int.TryParse(nummer, out siffra);
+//     if (siffra > 5)
+//     {
+//         Console.WriteLine("högre än 5!");
+//     }
+// }
+
+
+while ()
+{
+    
+}
+
+Console.ReadLine();
